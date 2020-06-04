f1a35c845f1b1ff74ea96fee5524d24db671f81f
NicolasCurtis <idk.curtis@gmail.com>
2020-06-04 16:40:05 +0200

    map back

---
+++ model/shema.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+
+var restoSchema = mongoose.Schema({
+    restoName: String,
+    longitude: Number,
+    latitude: Number
+});
+
+var restoModel = mongoose.model('restaurant', restoSchema)
+
+module.exports = restoModel;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
+var restoModel = require('../model/shema');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/mapScreens',async function(req, res, next) {
+  var newResto = new restoModel ({
+    restoName: "L'assiette du parc",
+    longitude: 45.7729772, 
+    latitude: 4.8383089
+   });
+
+   var resto = await newResto.save();
+   if (resto){
+     res.json({resto})
+   } else {
+     res.json({result:false})
+   }
+  //  var resto = await restoModel.find();
+});
+
+router.get('/restolist', async function(req, res, next) {
+    var restaurant = await restoModel.find()
+
+    if (restaurant){
+      res.json({restaurant})
+    } else {
+      res.json({result:false})
+    }
+});
 module.exports = router;
