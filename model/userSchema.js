f1e552927a76cf2466089dce74b3ffcdff9b0175
NicolasCurtis <idk.curtis@gmail.com>
2020-06-09 16:25:32 +0200

    nex

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose')
 
 var userSchema = mongoose.Schema({
-<<<<<<< HEAD
     prenom: String,
     nom: String,
     email: String,
@@ -11,14 +10,3 @@ var userSchema = mongoose.Schema({
 var usersModel = mongoose.model('users', userSchema)
 
 module.exports = usersModel;
-=======
-    prenom : String,
-    nom: String,
-    email: String,
-    mdp: String
-})
-
-var userModel = mongoose.model('users', userSchema)
-
-module.exports =  userModel;
->>>>>>> bb7f00379cc644b61ddc3b0675880be251d2f566
